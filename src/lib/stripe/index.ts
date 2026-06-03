f83d4d7d1e04a7616d5057e30846ebe43689dfa4
opidnebesnyi <podnestrade@gmail.com>
2026-06-03 11:27:38 +0200

    fix stripe version

--- src/lib/stripe/index.ts
+++ src/lib/stripe/index.ts
@@ -2,7 +2,7 @@ import Stripe from "stripe";
 
 export const stripe = process.env.STRIPE_SECRET_KEY
   ? new Stripe(process.env.STRIPE_SECRET_KEY, {
-      apiVersion: "2026-05-27.dahlia",
+      apiVersion: "2026-04-22.dahlia",
       typescript: true,
     })
   : null;
